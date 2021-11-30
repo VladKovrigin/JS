f85a6aab459b670c87c21587c714b812ad0d7e43
Vlad Kovrigin <vkov8212@gmail.com>
2021-11-30 22:22:05 +0200

    4-15 points

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ function truncate(str, maxLength) {
 
 //3. Напишите функцию getShortName(fullName), которая преобразует полное ФИО fullName в краткое.
 // Например getShortName("Иванов Сергей Петрович"), функция должна вернуть "Иванов С.П."
-
 function getShortName(fullName) {
     fullName = fullName.split(' ');
     return `${fullName[0]} ${fullName[1][0]}. ${fullName[2][0]}.`;
@@ -28,4 +27,229 @@ function getShortName(fullName) {
 
 //4. Напишите функцию, которая принимает массив объектов.
 // Функция должна вернуть массив только с теми объектами, у которых есть свойство name
+function checkName(objectArray) {
+    let newArray;
+    for(let item in objectArray) {
+        if(objectArray[item].hasOwnProperty('name')) {
+            newArray += objectArray;
+        }
+    } return newArray;
+}
 
+
+//5. Напишите функцию, которая принимает два массива. Функция должна
+// вернуть результат объединения этих массивов без дубликатов
+function unionArrays(arrayFirst, arraySecond) {
+    for(let item of arrayFirst) {
+        for(let value of arraySecond) {
+            if(arrayFirst[item] === arraySecond[value]) {
+                arraySecond.splice(value, 1);
+            }
+        }
+    }
+    return arrayFirst.concat(arraySecond);
+}
+
+
+//6. Напишите функцию, которая преобразует строки snake_case в camelCase.
+// Например modifyString('super-user-name') // вернуть 'superUserName'
+function toCamelCase(str) {
+    str = str.split('-');
+    str = str.map((word, index) => index === 0 ? word :
+            word[0].toUpperCase() + word.slice(1));
+    str = str.join('');
+    return str;
+}
+alert(toCamelCase('код-с-тире'));
+
+
+//7. Напишите функцию, которая принимает значение с математическим
+// выражением и возвращает результат его решения. Например:
+// calc('5 + 10') // вернет 15
+function calc(string) {
+    string = string.split(' ');
+    if(string[1] === '+') {
+        return +string[0] + +string[2];
+    }
+    if(string[1] === '-') {
+        return +string[0] - +string[2];
+    }
+    if(string[1] === '/') {
+        return +string[0] / +string[2];
+    }
+    if(string[1] === '*') {
+        return +string[0] * +string[2];
+    }
+    return 'что-то не так';
+}
+alert(calc('5 + 10'));
+
+
+//8. Напишите функцию, которая принимает объект с пользователями
+// и возвращает массив имён.
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        age: 20
+    },
+    {
+        id: 2,
+        name: 'Marry',
+        age: 22
+    },
+    {
+        id: 3,
+        name: 'Poll',
+        age: 25
+    },
+]
+function usersNames(users) {
+    let names = [];
+    for(let item in users) {
+        names[item] = users[item].name;
+    }
+    return names;
+}
+alert(usersNames(users));
+
+
+// 9. Напишите функцию, которая принимает массив. Каждый элемент
+// массива, который является числом - умножить на 2, строки -
+// преобразовать в массив, где каждый символ - отдельный элемент
+// массива. Функция должна вернуть измененный объект
+function changeArray(array) {
+    for(let item in array) {
+        if(isFinite(array[item])) {
+            array[item] = +array[item] * 2;
+        } else {
+            array[item] = array[item].split('');
+        }
+    }
+    return array;
+}
+let array = [1234, 'qwerty'];
+console.log(changeArray(array));
+
+
+// 10. Напишите функцию, которая проверяет строку на спам.
+// Функция должна возвращать true, если в строке есть 2
+// одинаковых слова, и false - если все слова уникальны.
+function checkSpam(string) {
+    string = string.split(' ');
+    return new Set(string).size !== string.length;
+}
+
+
+//11.Напишите функцию, которая проверяет строку на нецензурную лексику.
+// Создайте массив, который будет содержать слова, которые нельзя пропускать.
+// Функция должна принимать строку и проверять наличие в ней слов из
+// массива, которые нужно пропускать. Если в строке есть слово из массива
+// - вернуть true, если нет - false.
+const swearWords = ['блять', 'ебать', 'пизда', 'сука', 'хуй'];
+function checkSwearWords(string) {
+    string.split('');
+    for(let item of string) {
+        for(let word of swearWords) {
+            if(string[item] === swearWords[word]) {
+                console.log('и где тут мат? ' + string[item] + ' '+ swearWords[word])
+                return true;
+            }
+        }
+    }
+    return false;
+}
+alert('нет мата: ' + checkSwearWords('ого как неожиданно и приятно'));
+alert('есть мат: ' + checkSwearWords('ого ебать сука'));
+
+
+//12. Напишите функцию, которая принимает массив. Функция должна вернуть
+// предложение только из строковых значений массива. Например:
+// arrayToSentence(['Привет', 1, null, 'как', '{}', 'дела']) // вернуть Привет как дела
+function arrayToSentence(array) {
+    let newString = '';
+    for(let item of array) {
+        if(typeof array[item] !== 'object' && typeof array[item] !== 'number') {
+            newString += `${array[item]} `;
+            console.log(newString);
+        }
+    }
+    return newString;
+}
+
+
+console.log(arrayToSentence(['Привет', 1, null, 'как', '{}', 'дела']));
+
+//13. Напишите функцию, которая принимает массив из 10 целых чисел
+// (от 0 до 9), который возвращает строку этих чисел в форме номера телефона.
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+function createPhoneNumber(numbers){
+    let format = '(xxx) xxx-xxxx';
+
+    for (let number of numbers) {
+        format = format.replace('x', number);
+    }
+
+    return format;
+}
+
+//14. Напишите функцию, которая принимает массив с пользователями и возвращает
+// имя пользователя с наибольшей заработной платой.
+const usersSecond = [
+    {
+        id: 1,
+        name: 'John',
+        age: 20,
+        salary: 1000
+    },
+    {
+        id: 2,
+        name: 'Marry',
+        age: 22,
+        salary: 1500
+    },
+    {
+        id: 3,
+        name: 'Poll',
+        age: 25,
+        salary: 1200
+    },
+]
+function getHighestSalary(users) {
+    let maxSalary = users[0].salary;
+    let richest = '';
+    for(let item in users) {
+        if(users[item].salary > maxSalary) {
+            maxSalary = users[item].salary;
+            richest = users[item].name;
+        }
+    }
+    return richest;
+}
+
+console.log(`богатейший из всех ${getHighestSalary(usersSecond)}`);
+
+//15. Напишите функцию, которая проверяет корректность строки.
+// Строка должна быть не менее 3х символов и не более 16ти
+// символов, должна быть не пустая, не должна содержать числа.
+function checkCorrectString(string) {
+    if(string.trim()) {
+        return false;
+    }
+    if(string.length < 3 || string.length > 16) {
+        return false;
+    }
+    for(let symbol of string) {
+        if(isFinite(+string[symbol]) && !isNaN(+string[symbol])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log('в строке всё ок ' + checkCorrectString('тут всё ок'));
+console.log('вот тут число ' + checkCorrectString('тут число 1'));
+console.log('тут слишком мало символов ' + checkCorrectString('т'));
+console.log('слишком много символов ' + checkCorrectString('тут много символов очень много'));
+console.log('пустая строка ' + checkCorrectString('    '));
