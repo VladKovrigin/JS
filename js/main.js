5921fac2cf47c699f1f816da9343b77996ad5df7
Vlad Kovrigin <vkov8212@gmail.com>
2021-12-06 09:21:29 +0200

    first 10 points

--- js/main.js
+++ js/main.js
@@ -1,279 +1,174 @@
+//2. Создайте кнопку и инпут с текстом Hello, Palmo. При клике
+// на кнопку в инпуте текст должен замениться на Hello, World!
+let input = document.getElementById("second-point");
+input.value = 'Hello, Palmo';
 
-//1.Напишите функцию upFirst(str), которая возвращает строку str с заглавным первым символом,
-//    остальные символы должны быть в нижнем регистре (пример: upFirst("иванов"),
-//    должно вернуть "Иванов", up_first("ИВАНОВ"), должно вернуть "Иванов")
-function upFirst(str) {
-    if(str.length < 2) {
-        return str;
-    }
-    str = str.toLowerCase();
-    let firstLetter = str[0].toUpperCase();
-    return firstLetter + str.substr(1);
-}
-
-//2. Напишите функцию truncate(str, maxlength), которая проверяет длину строки str,
-// и если она превосходит maxlength – заменяет конец str на "...", так чтобы ее длина стала равна maxlength
-function truncate(str, maxLength) {
-    if(str.length > maxLength) {
-        return str.slice(0, maxLength) + '...';
-    }
-    return str;
+document.getElementById('to-hello-world').onclick = function() {
+    input.value = 'Hello, World';
 }
 
 
-//3. Напишите функцию getShortName(fullName), которая преобразует полное ФИО fullName в краткое.
-// Например getShortName("Иванов Сергей Петрович"), функция должна вернуть "Иванов С.П."
-function getShortName(fullName) {
-    fullName = fullName.split(' ');
-    return `${fullName[0]} ${fullName[1][0]}. ${fullName[2][0]}.`;
-}
+//3. Создайте кнопку и инпут. При клике на кнопку в
+// console.log выведите содержимое инпута.
+let inputSecond = document.getElementById("value-to-log");
 
-//4. Напишите функцию, которая принимает массив объектов.
-// Функция должна вернуть массив только с теми объектами, у которых есть свойство name
-function checkName(objectArray) {
-    let newArray = [];
-    for(let item in objectArray) {
-        if(objectArray[item].hasOwnProperty('name')) {
-            newArray.push(objectArray[item]);
-        }
-    }
-    return newArray;
+document.getElementById('console-log').onclick = function() {
+    console.log(inputSecond.value);
 }
 
 
-//5. Напишите функцию, которая принимает два массива. Функция должна
-// вернуть результат объединения этих массивов без дубликатов
-function unionArrays(arrayFirst, arraySecond) {
-    let result = [];
-    const fullArray = arrayFirst.concat(arraySecond);
+//4. Создайте 2 заголовка h1 и кнопку. При клике на кнопку текст
+// в заголовках должен замениться друг на друга.
+let reverseButton = document.getElementById("reverse-value");
+let firstValue = document.getElementById('first-value');
+let secondValue = document.getElementById('second-value');
 
-    for(let item of fullArray) {
-        if(!result.includes(item)) {
-            result.push(item);
-        }
-    }
-    return result;
+reverseButton.onclick = function() {
+    let value = firstValue.innerText;
+    firstValue.innerText = secondValue.innerText;
+    secondValue.innerText = value;
 }
 
 
-//6. Напишите функцию, которая преобразует строки snake_case в camelCase.
-// Например modifyString('super-user-name') // вернуть 'superUserName'
-function toCamelCase(str) {
-    str = str.split('-');
-    str = str.map((word, index) => index === 0 ? word :
-            word[0].toUpperCase() + word.slice(1));
-    str = str.join('');
-    return str;
-}
-alert(toCamelCase('код-с-тире'));
+//5. Создайте абзац (<p>) с любым текстом и кнопку.
+// При клике на кнопку цвет текста должен поменять на
+// красный, кликнув еще раз, текст должен вернуть исходный цвет.
+let buttonChangeP = document.getElementById('change-color');
+let pChangeColor = document.getElementById('p-color');
+let thisColor = false;
 
-
-//7. Напишите функцию, которая принимает значение с математическим
-// выражением и возвращает результат его решения. Например:
-// calc('5 + 10') // вернет 15
-function calc(string) {
-    string = string.split(' ');
-    if(isNaN(string[0]) || isNaN(string[2])) {
-        return 'ошибка';
+buttonChangeP.onclick = function() {
+    thisColor = !thisColor;
+    if(thisColor) {
+        pChangeColor.style.color = 'red';
     }
-    if(string[1] === '+') {
-        return +string[0] + +string[2];
-    }
-    if(string[1] === '-') {
-        return +string[0] - +string[2];
-    }
-    if(string[1] === '/') {
-        return +string[0] / +string[2];
-    }
-    if(string[1] === '*') {
-        return +string[0] * +string[2];
-    }
-    return 'что-то не так';
-}
-alert(calc('5 + 10'));
+    else {
+        pChangeColor.style.color = 'black';
 
-
-//8. Напишите функцию, которая принимает объект с пользователями
-// и возвращает массив имён.
-const users = [
-    {
-        id: 1,
-        name: 'John',
-        age: 20
-    },
-    {
-        id: 2,
-        name: 'Marry',
-        age: 22
-    },
-    {
-        id: 3,
-        name: 'Poll',
-        age: 25
-    },
-]
-function usersNames(users) {
-    let names = [];
-    for(let item in users) {
-        if(users[item].hasOwnProperty('name')) {
-            names[item] = users[item].name;
-        }
     }
-    return names;
-}
-alert(usersNames(users));
-
-
-// 9. Напишите функцию, которая принимает массив. Каждый элемент
-// массива, который является числом - умножить на 2, строки -
-// преобразовать в массив, где каждый символ - отдельный элемент
-// массива. Функция должна вернуть измененный объект
-function changeArray(array) {
-    for(let item in array) {
-        if(isFinite(array[item])) {
-            array[item] = +array[item] * 2;
-        } else {
-            array[item] = array[item].split('');
-        }
-    }
-    return array;
-}
-let array = [1234, 'qwerty'];
-console.log(changeArray(array));
-
-
-// 10. Напишите функцию, которая проверяет строку на спам.
-// Функция должна возвращать true, если в строке есть 2
-// одинаковых слова, и false - если все слова уникальны.
-function checkSpam(string) {
-    string = string.split(' ');
-    return new Set(string).size !== string.length;
 }
 
 
-//11.Напишите функцию, которая проверяет строку на нецензурную лексику.
-// Создайте массив, который будет содержать слова, которые нельзя пропускать.
-// Функция должна принимать строку и проверять наличие в ней слов из
-// массива, которые нужно пропускать. Если в строке есть слово из массива
-// - вернуть true, если нет - false.
-const swearWords = ['блять', 'ебать', 'пизда', 'сука', 'хуй'];
-function checkSwearWords(string) {
-    string = string.split(' ');
-    for(let item of string) {
-        if(swearWords.includes(item)) {
-            return true;
-        }
-    }
-    return false;
-}
-alert('нет мата: ' + checkSwearWords('ого как неожиданно и приятно'));
-alert('есть мат: ' + checkSwearWords('ого ебать сука'));
+//6. Создайте кнопку и инпут. При клике на кнопку инпут должен стать
+// заблокированным (disabled). При повторном клике должен разблокироваться.
+let buttonDisabled = document.getElementById('to-disabled');
+let disableInput = document.getElementById('disable-input');
+let disable = false;
 
-
-//12. Напишите функцию, которая принимает массив. Функция должна вернуть
-// предложение только из строковых значений массива. Например:
-// arrayToSentence(['Привет', 1, null, 'как', '{}', 'дела']) // вернуть Привет как дела
-function arrayToSentence(array) {
-    let newString = '';
-    for(let item of array) {
-        if(typeof item === 'string') {
-            newString += `${item} `;
-        }
-    }
-    return newString;
+buttonDisabled.onclick = function() {
+    disable = !disable;
+    disableInput.disabled = disable;
 }
 
-console.log(arrayToSentence(['Привет', 1, null, 'как', {}, 'дела']));
 
-//13. Напишите функцию, которая принимает массив из 10 целых чисел
-// (от 0 до 9), который возвращает строку этих чисел в форме номера телефона.
-// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
-function createPhoneNumber(numbers){
-    let format = '(xxx) xxx-xxxx';
+//7. Создайте 2 квадрата (зеленый и красный). При клике на зеленый
+// - цвет зеленого квадрата должен стать красным, красный - зеленым. И наоборот.
+let firstColor = document.getElementById('first');
+let secondColor = document.getElementById('second');
 
-    for (let number of numbers) {
-        format = format.replace('x', number);
-    }
+firstColor.style.background = 'red';
+secondColor.style.background = 'green';
 
-    return format;
+firstColor.onclick = function changeBackground() {
+    let color =  firstColor.style.background;
+    firstColor.style.background = secondColor.style.background;
+    secondColor.style.background = color;
 }
 
-//14. Напишите функцию, которая принимает массив с пользователями и возвращает
-// имя пользователя с наибольшей заработной платой.
-const usersSecond = [
-    {
-        id: 1,
-        name: 'John',
-        age: 20,
-        salary: 1000
-    },
-    {
-        id: 2,
-        name: 'Marry',
-        age: 22,
-        salary: 1500
-    },
-    {
-        id: 3,
-        name: 'Poll',
-        age: 25,
-        salary: 1200
-    },
-]
-function getHighestSalary(users) {
-    let maxSalary = users[0].salary;
-    let richest = '';
-    for(let item in users) {
-        if(users[item].salary > maxSalary) {
-            maxSalary = users[item].salary;
-            richest = users[item].name;
-        }
-    }
-    return richest;
+secondColor.onclick = function changeBackground() {
+    let color =  firstColor.style.background;
+    firstColor.style.background = secondColor.style.background;
+    secondColor.style.background = color;
 }
 
-console.log(`богатейший из всех ${getHighestSalary(usersSecond)}`);
 
-
-//15. Напишите функцию, которая проверяет корректность строки.
-// Строка должна быть не менее 3х символов и не более 16ти
-// символов, должна быть не пустая, не должна содержать числа.
-function checkCorrectString(string) {
-    if(typeof string !== 'string')  {
-        return false;
-    }
-    string = string.trim();
-    if(string.length < 3 || string.length > 16) {
-        return false;
-    }
-    for(let item of string) {
-        if( item !== ' '  && !isNaN(+item) ) {
-            return false;
-        }
-    }
-    return true;
+//8. Создайте кнопку и пустой список ul на странице. При каждом клике
+// должен появляться новый элемент списка с порядковым номером.
+let addLi = document.getElementById('add-li');
+let myUl = document.getElementById('ol');
+let item = 0;
+addLi.onclick = function() {
+    let createLi = document.createElement('li');
+    createLi.innerText = item;
+    myUl.prepend(createLi);
+    item++;
 }
 
-//16. Напишите функцию, которая проверяет пароль на корректность.
-// Пароль должен быть не менее 6ти символов, должен содержать
-// хотя бы одну цифру и хотя бы одну заглавную букву
-function checkCorrectPassword(password) {
-    let truthFirst = false;
-    let truthSecond = false;
-    password = password.split('');
-    if(password.length >= 6) {
-        for(let item of password) {
-            if(item === item.toUpperCase()) {
-                truthFirst = true;
-            }
-            if(item !== ' '  && !isNaN(+item)) {
-                truthSecond = true;
-            }
-        }
+
+//9. Создайте textarea и кнопку на странице. При клике на кнопку
+// внизу должен появиться список с каждым значением в textarea,
+// перечисленном через запятую. Например: в textarea ввели -
+// яблоко, груша, помидор, апельсин, манго. Внизу должен появиться список
+//         1. яблоко
+//         2. груша
+//         3. помидор
+//         4. апельсин
+//         5. манго
+let addList = document.getElementById('add-to-textarea');
+let fromTextArea = document.getElementById('from-textarea');
+
+addList.onclick = function() {
+    let text = document.getElementById('text-area').value;
+    text = text.split(',');
+    let i = 1;
+    for(let item of text) {
+        let createLi = document.createElement('li');
+        createLi.innerText = `${i}. ${item}`;
+        fromTextArea.append(createLi);
+        i++;
     }
-    if(truthSecond && truthSecond) {
-        return true;
-    } else {
-        return false;
+}
+
+
+// форма введения логина, пароля, повтора пароля, емейла и возраста
+let submitButton = document.getElementById('submit-button');
+let login = document.getElementById('login');
+let password = document.getElementById('password');
+let passwordAgain = document.getElementById('password-again');
+let email = document.getElementById('email');
+let myAge = document.getElementById('age');
+let checkLogin = '._/\\|,';
+submitButton.disabled = false;
+let check = 0;
+submitButton.onclick = function () {
+    if(login.value.length < 4 || login.value.length > 20 || login.value.includes(checkLogin)) {
+        check++;
     }
+
+    let checkEmail = /\S+@\S+\.\S+/;
+    if(!checkEmail.test(email.value)) {
+        check++;
+    }
+    if(!isFinite(myAge.value) || isFinite(myAge.value) < 0) {
+        check++;
+    }
+    let checkPassword = password.value.match(/\d+/);
+    if(password.value.length < 6 || !checkPassword) {
+        check++;
+    }
+    let checkPasswordAgain = passwordAgain.value.match(/\d+/);
+    if(passwordAgain.value.length < 6 || !checkPasswordAgain) {
+        check++;
+    }
+    if(password.value === passwordAgain.value) {
+        check++;
+    }
+
+    if(check > 0) {
+        console.log(check);
+        alert('ПРОВЕРЬТЕ ПРАВИЛЬНОСТЬ ЗАПОЛНЕНИЯ');
+    }
+}
+
+//11. Создайте простейший калькулятор. На странице должны быть
+// кнопки от 0 до 9 и знаки + - * / =. При клике на каждую цифру
+// или знак, значение должно появляться в строке. При нажатии на
+// кнопку "=" - вывести результат математического выражения.
+// Не забудьте добавить все проверки.
+let button = document.getElementById('keyboard').getElementsByTagName('button');
+let previous = document.getElementById('previous').getElementsByTagName('p');
+let result = document.getElementById('result').getElementsByTagName('p');
+
+button.onclick = function() {
+    previous.prepend(button.value);
 }
