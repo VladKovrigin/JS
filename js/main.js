0675279d459c860b3b50a80711464beb1be31e71
Vlad Kovrigin <vkov8212@gmail.com>
2021-12-10 09:49:35 +0200

    add test, check correct answers and other features

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ const test = {};
 test.questions = [];
 let tests = [];
 let testNumber = 0;
-let questionCounter = 0;
 const testList = document.getElementById('test-list');
-
+let thisTest = 0;
+const listItem = document.getElementsByClassName('list-item');
 
 //Добавление поля варианта при создание вопроса
 addElement.onclick = () => {
     let options = document.getElementById('options');
-    console.log(options);
     options.innerHTML +=
         '<li>\n' +
         '    <input class="form-check-input check-correct-answer" type="checkbox">\n' +
@@ -26,23 +25,39 @@ deleteElement.onclick = function () {
 }
 
 // Функция для очистки страницы
-function clearAllQuestions() {
-    let question = document.getElementById('test');
-    while(questionCounter > 0) {
-        question.lastChild.remove();
-        questionCounter--;
+
+function clearAllQuestions ()  {
+    while(questionList.lastChild) {
+        questionList.lastChild.remove();
     }
-    return question;
+    deleteQuestion.style.visibility = 'hidden';
 }
 
+const addQuestion = document.getElementById('add-question');
+
+const checkCorrects = document.getElementById('check-corrects');
+const testName = document.getElementById('test-name');
 
 //Создание объекта нового теста
 const addTest = document.getElementById('add-test');
-addTest.onclick = clearAllQuestions();
+addTest.onclick = function addNewTest() {
+    clearAllQuestions();
+    myMark.style.visibility = 'hidden';
+
+    addQuestion.style.visibility = 'visible';
+    saveTest.style.visibility = 'visible';
+    checkCorrects.style.visibility = 'hidden';
+    testName.style.visibility = 'visible';
+    for(let thisTest of listItem) {
+        thisTest.style.pointerEvents = 'auto';
+    }
+
+    return false;
+}
+checkCorrects.style.visibility = 'hidden';
 
 const addAnswer = document.getElementsByClassName('add-answer');
 const addAnswerButton = document.getElementById('add-answer-button');
-const testName = document.getElementById('test-name');
 const questionName = document.getElementById('question-name');
 const addCorrect = document.getElementsByClassName('check-correct-answer');
 const questionList = document.getElementById('test');
@@ -67,7 +82,6 @@ addAnswerButton.onclick = () => {
     //Находим правильный вариант ответа из отмеченных
     [].forEach.call(addCorrect, (item) => {
         arrayCorrects.push(item.checked);
-        console.log('checked ' + item.checked);
     })
 
     //Добавляем на страницу варианты ответов
@@ -95,63 +109,127 @@ addAnswerButton.onclick = () => {
     //Добавляем вопрос на страницу
     test.questions.push(newQuestion);
     questionName.value = '';
-
-    questionCounter++;
+    deleteQuestion.style.visibility = 'visible';
 }
 
 
 //Удаление вопроса
 const deleteQuestion = document.getElementById('delete-question');
-deleteQuestion.onclick = function () {
+deleteQuestion.onclick = () => {
     let question = document.getElementById('test');
     question.lastChild.remove();
-
-    questionCounter--;
 }
 
 
 //Сохранение теста
-const saveQuestion = document.getElementById('save-test');
-saveQuestion.onclick = () => {
+const saveTest = document.getElementById('save-test');
 
-    test.name = `${testName.value}`;
-
+saveTest.onclick = () => {
     let testsInLS = JSON.parse(localStorage.getItem('tests'));
-    if(testsInLS === null) testsInLS = [];
-    testsInLS.push(test);
-    console.log(testsInLS);
-    localStorage.setItem('tests', JSON.stringify(testsInLS));
 
-    testList.innerHTML +=
-        `<li id="${testNumber++}" class="list-item" onclick="getTest(this, testNumber)">${testName.value}</li>`;
+    if(testName.value && +test.questions.length > 0) {
+        test.name = `${testName.value}`;
+        if(testsInLS === null) testsInLS = [];
+        testsInLS.push(test);
+        localStorage.setItem('tests', JSON.stringify(testsInLS));
 
-    clearAllQuestions();
+        testList.innerHTML +=
+            `<li id="${testNumber++}" class="list-item" onclick="getTest(this, testNumber)">${testName.value}</li>`;
+
+        addQuestion.style.visibility = 'hidden';
+        saveTest.style.visibility = 'hidden';
+        clearAllQuestions();
+    }
+}
+let testsInLS = JSON.parse(localStorage.getItem('tests'));
+
+if(JSON.parse(localStorage.getItem('tests'))) {
+    testsInLS.forEach((test) => {
+        testList.innerHTML +=
+            `<li id="${testNumber++}" class="list-item" onclick="getTest(this)">${test.name}</li>`;
+    })
 }
 
-function getTest(elem, testNum) {
+
+function getTest(elem) {
+    // Корректируем страницу
     clearAllQuestions();
-    console.log(elem);
+    addQuestion.style.visibility = 'hidden';
+    deleteQuestion.style.visibility = 'hidden';
+    saveTest.style.visibility = 'hidden';
+    let tests = JSON.parse(localStorage.getItem('tests'));
+    thisTest = tests[elem.id];
 
-    let tests = JSON.parse(localStorage.getItem('test'));
-    let thisTest = tests[testNum];
+    //Добавляем на страницу название теста
+    questionList.innerHTML +=
+        '<div class="form-check">\n' +
+        '    <p id="question-title">' +
+        `        ${thisTest.name}` +
+        '    </p>' +
+        '</div>';
 
-    //Добавляем на страницу сам вопрос
-    tests[testNum].forEach((myTest) => {
+    // Выводим вопрос
+    for(let item = 0; item < thisTest.questions.length; item++) {
+        //Сам вопрос
         questionList.innerHTML +=
             '<div class="form-check">\n' +
             '    <p id="question-title">' +
-            `        ${myTest.name}` +
+            `        ${thisTest.questions[item].title}` +
             '    </p>' +
             '</div>';
 
-        myTest.questions.forEach((question) => {
+        //Варианты ответов
+        thisTest.questions[item].answers.forEach((answer) => {
             questionList.lastChild.innerHTML +=
-                '     <div>\n' +
+                '     <div class="right-answer">\n' +
                 '        <label class="form-check-label">\n' +
                 '            <input class="form-check-input" type="checkbox" name="exampleRadios">\n' +
-                `                 ${question}\n` +
+                `                 ${answer.text}\n` +
                 '        </label>\n' +
                 '    </div>\n';
         })
-    })
+    }
+
+    // можем кликать на все тесты кроме выбранного
+    for(let thisTest of listItem) {
+        thisTest.style.pointerEvents = 'auto';
+    }
+    elem.style.pointerEvents = 'none';
+
+    // Корректируем страницу
+    checkCorrects.style.visibility = 'visible';
+    testName.value = '';
+    testName.style.visibility = 'hidden';
+}
+
+const myMark = document.getElementById('mark');
+
+checkCorrects.onclick = () => {
+
+    let inputs = document.getElementsByClassName('form-check-input');
+    let div = document.getElementsByClassName('right-answer');
+    let i = 0;
+    let mark = 0;
+    let maxMark = 0;
+
+    //Проверка на правильность
+    for(let item of thisTest.questions) {
+        for(let ans of item.answers) {
+            if(ans.isCorrect && inputs[i].checked) {
+                div[i].style.backgroundColor = 'rgba(83, 185, 66, 0.4)';
+                mark++;
+                maxMark++;
+            }
+            if(!ans.isCorrect && inputs[i].checked) {
+                div[i].style.backgroundColor = 'rgba(255,69,50,0.4)';
+            }
+            if(ans.isCorrect && !inputs[i].checked) {
+                div[i].style.backgroundColor = 'rgba(255,153,50,0.4)';
+                maxMark++;
+            }
+            inputs[i].style.display = 'none';
+            i++;
+        }
+    }
+    myMark.innerHTML += `<p id="my-mark">Баллов: ${mark}/${maxMark}</p>\n`;
 }
